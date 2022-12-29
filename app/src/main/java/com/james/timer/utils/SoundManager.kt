4ecd4ba961b4735592db769a9be77e9840f7b0e8
james.lin <james.lin@binance.com>
2022-12-29 19:55:08 +0800

    [fix]: Duplicated Activity launched.

--- app/src/main/java/com/james/timer/utils/SoundManager.kt
+++ app/src/main/java/com/james/timer/utils/SoundManager.kt
@@ -16,16 +16,13 @@ class SoundManager @Inject constructor(@ApplicationContext private val context: 
     private val mediaPlayer: MediaPlayer by lazy {
         return@lazy MediaPlayer().let { mp ->
             val ringtoneUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
-            try {
-                mp.setDataSource(context, ringtoneUri)
-                val attributes = AudioAttributes.Builder()
-                    .setLegacyStreamType(AudioManager.STREAM_ALARM)
-                    .build()
-                mp.setAudioAttributes(attributes)
-                mp.isLooping = true
-                mp.prepare()
-            } catch (e: Exception) {
-            }
+            mp.setDataSource(context, ringtoneUri)
+            val attributes = AudioAttributes.Builder()
+                .setLegacyStreamType(AudioManager.STREAM_ALARM)
+                .build()
+            mp.setAudioAttributes(attributes)
+            mp.isLooping = true
+            mp.prepare()
             mp
         }
     }
